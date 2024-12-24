0f3dea14edaa6ea97af0efb58ad3cf41ecbf52d1
Zélie Lemahieu <zelielemahieu@gmail.com>
2024-12-24 14:26:42 +0100

    first commit

---
+++ ExerciceLesSalaries_24_12_24/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.SymbolStore;
+using ExerciceLesSalaries_24_12_24.classes;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialisation des variables:
+            List<Salarie> salaries = new List<Salarie>();
+            double salaireTotalDeLEntreprise = 0;
+            double salaireMoyenDeLEntreprise = 0;
+
+            //Message de bienvenue:
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Bienvenue sur la portail \"Salarié.e.s\" de Zirgi Vopp Inc. (Antenne Terrestre)");
+            Console.ResetColor();
+
+            //Utilisation du constructeur pour instancier des salariés:
+            Salarie AdorpA = new Salarie("Adorp Aderpman", "Développement Gnorpien");
+            Salarie BorduB = new Salarie("Bordü Bolpville", "Développement Gnorpien", 100);
+            Salarie ConglupC = new Salarie("Conglup de Congdorp", "Ressources Aliens", 300);
+            Salarie AlisonW = new Salarie("Alison Wheeler", "Département Clown", 89000);
+            Salarie DerpD = new Salarie("Derp Derpnaught", "Ressources Aliens", 200);
+            Salarie ErnpE = new Salarie("Ernp Ermanerp", "Développement Gnorpien");
+            Salarie salarie1;
+
+            Console.WriteLine("Entrer nouveau salarié : nom, service, salaire :");
+            salarie1 = new Salarie(Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine()));
+
+            //Ajout des salariés à la liste des salariés:
+            salaries.Add(AdorpA);
+            salaries.Add(BorduB);
+            salaries.Add(ConglupC);
+            salaries.Add(AlisonW);
+            salaries.Add(DerpD);
+            salaries.Add(ErnpE);
+            salaries.Add(salarie1);
+
+            //Affichage des infos sur l'entreprise:
+            InformationGlobalesDeLEntreprise();
+            Console.WriteLine("");//<--Passage de ligne.
+            Equipe();
+            Console.ResetColor();
+            Reinitialisation();
+
+
+            //FONCTION INFORMATION GLOBALES DE L'ENTREPRISE:
+            void InformationGlobalesDeLEntreprise()
+            {
+                foreach(Salarie salarie in salaries)//<--Calcul de la somme des salaires de l'entreprise.
+                {
+                    salaireTotalDeLEntreprise += salarie.Salaire;
+                };
+                salaireMoyenDeLEntreprise /= salaries.Count;//<--Calcul de la moyenne des salaires de l'entreprise.
+
+                Titrage("INFORMATION GLOBALES SUR L'ENTREPRISE");
+                Console.WriteLine($"Nombre de salarié.e.s : {salaries.Count}" +
+                    $"\nSalaire Totale : {salaireTotalDeLEntreprise} glimbs/mois" +
+                    $"\nSalaire Moyen : {salaireMoyenDeLEntreprise} glimbs/mois");
+            }
+
+            //FONCTION EQUIPE
+            void Equipe()
+            {
+                Titrage("NOTRE ÉQUIPE");
+                foreach (Salarie salarie in salaries)
+                {
+                    Console.WriteLine($"{salarie.Nom}, du service {salarie.Service}, gagne {salarie.Salaire} glimbs/mois.");
+                }
+            }
+
+            //FONCTION RÉINITIALISATION
+            void Reinitialisation()
+            {
+                salaireMoyenDeLEntreprise = 0;
+                salaireTotalDeLEntreprise = 0;
+            }
+
+            //FONCTION TITRAGE:
+            //Affichage d'un titre encadré en jaune
+            void Titrage(string titre)
+            {
+                titre = $"║ {titre} ║";
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("╔");
+                for (int i = 0; i < titre.Length - 2; i++)
+                {
+                    Console.Write("═");
+                };
+                Console.WriteLine("╗");
+                Console.WriteLine(titre);
+                Console.Write("╚");
+                for (int i = 0; i < titre.Length - 2; i++)
+                {
+                    Console.Write("═");
+                };
+                Console.WriteLine("╝");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+        }
+    }
+}
---
+++ ExerciceLesSalaries_24_12_24/classes/Salarie.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceLesSalaries_24_12_24.classes
+{
+    internal class Salarie
+    {
+        //ATTRIBUTS:
+        private String _nom;
+        private String _service;
+        private double _salaire;
+
+        //PROPRIÉTÉS:
+        public String Nom { get { return _nom; } set { _nom = value; } }
+        public String Service { get { return _service; } set { _service = value; } }
+        public double Salaire { get { return _salaire; } set { if (value < 100 || value > 100000) { throw new ArgumentOutOfRangeException("Le salaire doit être supérieur au SMIC de Virzoble (0 glimb)"); } _salaire = value; } }
+
+        //CONSTRUCTEUR:
+        public Salarie(String nom, String service, double salaire)
+        {
+            Nom = nom;
+            Service = service;
+            Salaire = salaire;
+        }
+
+        //CONSTRUCTEUR SALAIRE PAR DÉFAUT:
+        public Salarie(String nom, String service)
+        {
+            Nom = nom;
+            Service = service;
+            Salaire = 100;
+        }
+
+        //CONSTRUCTEUR PAR DÉFAUT:
+        public Salarie()
+        {
+            Nom = "Alan Smithee";
+            Service = "Global Vorping";
+            Salaire = 100;
+        }
+
+        //MÉTHODE AFFICHAGE SALAIRE:
+        public void AffichageDuSalaire(Salarie salarie)
+        {
+            Console.WriteLine($"Le.a salarié.e {Nom} gagne un salaire mensuel de {salarie.Salaire} glinbs");
+        }
+
+        //MÉTHODE RÉINITIALISER:
+        public void Reinitilisation()
+        {
+            Nom = "";
+            Service = "";
+            Salaire = 100;
+        }
+    }
+}
+
